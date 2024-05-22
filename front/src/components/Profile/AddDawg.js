8a7c423d84c204b75f3d47b376e3131f7a498dfa
TAHA202020 <tahajayche@gmail.com>
2024-05-22 20:47:25 +0200

    createed databse for dawgs + workign on images upload for dogs images

--- front/src/components/Profile/AddDawg.js
+++ front/src/components/Profile/AddDawg.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function AddDawg() {
-  return (<div>Add Dawg
+  const[imagesObject,setImagesObject]=useState([])
+  return (<div>
+    Add Dawg
+    <div className='small-images'>
+      {imagesObject.map((image,index)=>{return <img src={image} alt='dawg' key={index}/>})}
+    </div>
+    <label for="images">
+      <span>Choisir une image</span>
+      <input type='file' id='images' accept='image/*' onChange={(e)=>{setImagesObject([...imagesObject,URL.createObjectURL(e.target.files[0])])}} multiple />
+    </label>
   </div>
   )
 }
