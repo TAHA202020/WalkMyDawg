1d443b2796d58db1b75c9ff5590844f861bbd090
TAHA202020 <tahajayche@gmail.com>
2024-05-24 23:37:32 +0200

    added upload files and remove them before fetch **only Front**

--- front/src/components/Profile/AddDawg.js
+++ front/src/components/Profile/AddDawg.js
@@ -2,10 +2,15 @@ import React,{useState} from 'react'
 
 function AddDawg() {
   const[imagesObject,setImagesObject]=useState([])
-  return (<div>
-    Add Dawg
+  const removebyIndex=(index)=>{
+    URL.revokeObjectURL([imagesObject[index]])
+    imagesObject.splice(index,1)
+    setImagesObject([...imagesObject])
+    
+  }
+  return (<div className='add-dawg'>
     <div className='small-images'>
-      {imagesObject.map((image,index)=>{return <img src={image} alt='dawg' key={index}/>})}
+      {imagesObject.map((image,index)=>{return <div className='small-image-container'> <img src={image} alt='dawg' key={index} className='small-image'/> <button className='remove-image-btn' onClick={()=>{removebyIndex(index)}}>x</button></div>})}
     </div>
     <label for="images">
       <span>Choisir une image</span>
