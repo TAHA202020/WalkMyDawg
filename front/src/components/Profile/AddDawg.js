7e5a23862665d13d76d15cda6a352ed86956f797
TAHA202020 <tahajayche@gmail.com>
2024-05-24 23:52:13 +0200

    added css for add-dawg

--- front/src/components/Profile/AddDawg.js
+++ front/src/components/Profile/AddDawg.js
@@ -9,13 +9,26 @@ function AddDawg() {
     
   }
   return (<div className='add-dawg'>
+    <div className='flex'>
+
+    
+    <div className='add-dawg-images'>
+      <label for="name">Name your Dawg :</label>
+      <input  type='text'   placeholder='Name your Dawg'/>
+    </div>
+    <div className='add-dawg-images'>
+    <p className='small-images-label'>Add Dawg Images :</p>
     <div className='small-images'>
       {imagesObject.map((image,index)=>{return <div className='small-image-container'> <img src={image} alt='dawg' key={index} className='small-image'/> <button className='remove-image-btn' onClick={()=>{removebyIndex(index)}}>x</button></div>})}
     </div>
+    <div className='flex-images'>
     <label for="images">
       <span>Choisir une image</span>
       <input type='file' id='images' accept='image/*' onChange={(e)=>{setImagesObject([...imagesObject,URL.createObjectURL(e.target.files[0])])}} />
     </label>
+    </div>
+  </div>
+  </div>
   </div>
   )
 }
